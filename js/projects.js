2895846def395b3ae5c0259e1e639cb1c1e5bfa8
Victor0596647 <85453971+Victor0596647@users.noreply.github.com>
2022-04-18 21:02:22 +0000

    added icon

--- js/projects.js
+++ js/projects.js
@@ -4,15 +4,15 @@ var output = "";
 function init(){
     addProject("Php Account System",
     "https://github.com/Victor0596647/account-system-website/",
-    "/projects/img/php-account-system.png",
+    "projects/img/php-account-system.png",
     "I made a php application where you create an account, login, and remove the account. This isn't really interesting since it was for practice.");
     addProject("Catch The Block",
     "https://github.com/Victor0596647/CatchTheBlock",
-    "/projects/img/catchtheblock.png",
+    "projects/img/catchtheblock.png",
     "I made a game where the player has to move their mouse to the falling block to catch it. This was made using C++ and SFML.");
     addProject("Market Tycoon",
     "https://github.com/Victor0596647/PyShopTycoon",
-    "/projects/img/markettycoon.png",
+    "projects/img/markettycoon.png",
     "I made a text-based Market Tycoon game in python which will allow you to own a store and sell items. (Under Development)");
     // addProject("Textia RPG",
     // "https://github.com/Victor0596647/Textia-RPG",
