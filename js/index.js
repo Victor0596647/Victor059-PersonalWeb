484b58d0cd73f4e0374b0d10b3e36592d912ffb1
Victor0596647 <85453971+Victor0596647@users.noreply.github.com>
2022-05-01 14:37:53 +0000

    added cookies to switch

--- js/index.js
+++ js/index.js
@@ -1,23 +1,54 @@
 var switchCon = document.getElementById('switch'); 
 var toggle = document.getElementById('toggle');
 
-changeBg();
+if(getCookie('direction') == null){
+    setCookie('direction','left',14);
+}
+changeBg(getCookie('direction'));
 
-switchCon.addEventListener("click", () => changeBg());
+switchCon.addEventListener("click", () => checkBg());
 
-function changeBg(){
-    if(toggle.style.left == '0px'){
+function changeBg(dir){
+    if(dir == 'right'){
         toggle.style.left = '55px';
         toggle.style.backgroundImage = 'linear-gradient(0deg, #c31432,#c31432)';
         document.body.style.backgroundImage = 'linear-gradient(0deg, #c31432,#c31432)';
-        console.log('right');
-    }else{
+        document.body.style.animation = '';
+    }else if(dir == 'left'){
         toggle.style.left = '0px';
         toggle.style.backgroundImage = 'linear-gradient(-45deg, #ff0037, #8400ff)';
         document.body.style.backgroundImage = '-webkit-linear-gradient(-45deg, #200122,#6f0000, #ff0037, #8400ff, #23d5ab)';
         document.body.style.backgroundImage = 'linear-gradient(-45deg, #200122,#6f0000, #ff0037, #8400ff, #23d5ab)';
         document.body.style.backgroundSize = '400% 400%';
         document.body.style.animation = 'gradient 15s ease infinite';
-        console.log('left');
     }
+}
+
+function checkBg(){
+    if(getCookie('direction') == 'left'){
+        setCookie('direction','right',14);
+    }else if(getCookie('direction') == 'right'){
+        setCookie('direction','left',14);
+    }
+    changeBg(getCookie('direction'));
+}
+
+function setCookie(name, value, expire){
+    const date = new Date();
+    date.setTime(date.getTime() + (expire * 24 * 60 * 60 * 1000));
+    let expires = "expires=" + date.toUTCString();
+    document.cookie = `${name}=${value}; ${expires}; path=/`;
+}
+
+function getCookie(name){
+    const cDecoded = decodeURIComponent(document.cookie);
+    const arr = cDecoded.split("; ");
+    let result = null;
+
+    arr.forEach(element => {
+        if(element.indexOf(name) == 0){
+            result = element.substring(name.length + 1);
+        }
+    })
+    return result;
 }
